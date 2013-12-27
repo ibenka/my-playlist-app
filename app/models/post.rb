5041c4a35dfd5469b63f8a69310fffec76de9241
Justin Pinili <jro.bot.email@gmail.com>
2013-12-26 18:28:25 -0800

    Added validation methods to post, refactored post views to use a form partial.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,8 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :comments
 
   default_scope order('created_at DESC')
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :user, presence: true
 end
