21467e58164ad55cda050ba0b7e30b9e06a3f021
Justin Pinili <jro.bot.email@gmail.com>
2013-12-26 20:56:42 -0800

    Added Image to Post.

--- app/uploaders/image_uploader_uploader.rb
+++ app/uploaders/image_uploader_uploader.rb
@@ -1,13 +1,11 @@
 # encoding: utf-8
 
-class AvatarUploader < CarrierWave::Uploader::Base
+class ImageUploaderUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  #include Sprockets::Helpers::RailsHelper
-  
   # Choose what kind of storage to use for this uploader:
   # storage :file
   storage :fog
@@ -28,25 +26,25 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-  process :resize_to_fill => [200,200]
+  #
   # def scale(width, height)
   #   # do something
   # end
+  process :resize_to_fill => [300, 300]
 
+  version :thumb do
+    process :resize_to_fill => [50, 50]
+  end
   # Create different versions of your uploaded files:
-  version :tiny do
-    process :resize_to_fill => [20, 20]
-  end
-
-  version :small do
-    process :resize_to_fill => [30, 30]
-  end
+  # version :thumb do
+  #   process :scale => [50, 50]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
+  # def extension_white_list
+  #   %w(jpg jpeg gif png)
+  # end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
