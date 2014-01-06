464e3a10cd1b3ff4fb7762bfd6b9d85418fb6a8a
Justin Pinili <jro.bot.email@gmail.com>
2014-01-05 21:32:29 -0800

    Scoped topics for privacy

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -17,6 +17,7 @@ class Ability
         can :create, Vote
         can :create, Favorite, user_id: user.id
         can :destroy, Favorite, user_id: user.id
+        can :read, Topic
     end
 
     if user.role? :moderator
@@ -29,7 +30,8 @@ class Ability
         can :manage, :all
     end
 
-    can :read, :all
+    can :read, Topic, public: true
+    can :read, Post
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,6 @@ class Topic < ActiveRecord::Base
 
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 10 }, presence: true
+
+  scope :visible_to, lambda { |user| user ? scoped : where(public: true) }
 end
