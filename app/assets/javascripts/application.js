2dbfb9db8c2346ba93dc24e6c4c70d9522be5dac
Justin Pinili <jro.bot.email@gmail.com>
2014-01-08 21:25:44 -0800

    Created show / hide jQuery function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,16 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+  $(".js-show-hide").click(function() {
+    var selector = "." + $(this).attr('data-selector');
+    if ($(selector).is(":visible")) {
+      $(selector).slideUp();
+    }
+    else {
+      $(selector).slideDown();
+    }
+    return false;
+  });
+});
