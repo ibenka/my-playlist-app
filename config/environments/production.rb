1171fc671d86423f52ef5d5f28d041b4b239a88d
Justin Pinili <jro.bot.email@gmail.com>
2014-01-16 11:26:14 -0800

    corrected e-mailing URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ Bloccit::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   #http://justinpinili-bloccit.herokuapp.com
-  config.action_mailer.default_url_options = { :host => 'justinpinili-bloccit.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'my-playlist-app.herokuapp.com'}
 end
